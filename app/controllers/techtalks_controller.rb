bdb6bf408fc88962ee84a204689fca35c904c8ff
doislan <doislan@gmail.com>
2016-06-24 15:29:05 -0300

    search bar added

--- app/controllers/techtalks_controller.rb
+++ app/controllers/techtalks_controller.rb
@@ -4,24 +4,17 @@ class TechtalksController < ApplicationController
   
 	def index
 		if (request.query_parameters.any?)
-			if request.query_parameters.include? 'name'
-				filterByName
+			if request.query_parameters.include? 'keyword'
+				filter
 			elsif request.query_parameters.include? 'company_name'
 				filterByCompanyName
 			elsif request.query_parameters.include? 'begin_date'
 				filterByDate
-			elsif request.query_parameters.include? 'tag'
-				filterByTag
 			end		
 				
 		else
 			@techtalks = Techtalk.all
 		end
-	end
-
-	def filterByName
-		name = request.query_parameters['name']
-		@techtalks = Techtalk.where(name: name).all
 	end
 
 	def filterByCompanyName
@@ -32,12 +25,12 @@ class TechtalksController < ApplicationController
 	def filterByDate
 		begin_date = request.query_parameters['begin_date']
 		end_date = request.query_parameters['end_date']
-		@techtalks = Techtalk.where(['date >= ? and date <= ?', "%#{created_at}%", "%#{updated_at}%"]).all
+		@techtalks = Techtalk.where(['created_at BETWEEN ? and ?', "#{begin_date}%", "#{end_date}%"]).all
 	end
 
-	def filterByTag
-		begin_date = request.query_parameters['tag']
-		@techtalks = Techtalk.where(['tags LIKE ?', "%#{begin_date}%"])
+	def filter
+		param = request.query_parameters['keyword']
+		@techtalks = Techtalk.where(['tags LIKE ? OR name LIKE ? ', "%#{param}%", "%#{param}%"])
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'auth#callback'
   get '/auth/failure' => 'auth#failure'
 
+  get '/search' => 'techtalks#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
