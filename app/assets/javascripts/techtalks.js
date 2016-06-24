ece7fa8dcf46c60806d94cdf5b6c44ad7d790b59
emisanada <emisanada@hotmail.com>
2016-06-24 17:52:37 -0300

    card register

--- app/assets/javascripts/techtalks.js
+++ app/assets/javascripts/techtalks.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+(function($){
+  $(document).ready(function(){
+    // $('.datepicker').pickadate({
+    //   selectMonths: true, // Creates a dropdown to control month
+    //   selectYears: 15 // Creates a dropdown of 15 years to control year
+    // });
+  });
+})(jQuery)
